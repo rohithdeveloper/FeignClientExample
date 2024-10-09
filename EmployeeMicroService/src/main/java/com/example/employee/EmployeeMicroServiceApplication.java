d8fa97ccdc0ca8aa2d76a8a1f94176bfab42e59c
ROHITH PARIMELLA <87310735+rohithdeveloper@users.noreply.github.com>
2024-10-09 22:22:54 +0530

    Update EmployeeMicroServiceApplication.java

--- EmployeeMicroService/src/main/java/com/example/employee/EmployeeMicroServiceApplication.java
+++ EmployeeMicroService/src/main/java/com/example/employee/EmployeeMicroServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCaching
 public class EmployeeMicroServiceApplication {
 
 	public static void main(String[] args) {
