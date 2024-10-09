ece9df0bc0d56ce0a823efc1de2c0c1e25ca5600
ROHITH PARIMELLA <87310735+rohithdeveloper@users.noreply.github.com>
2024-10-09 22:31:21 +0530

    Update EmployeeServiceImpl.java

--- EmployeeMicroService/src/main/java/com/example/employee/serviceimpl/EmployeeServiceImpl.java
+++ EmployeeMicroService/src/main/java/com/example/employee/serviceimpl/EmployeeServiceImpl.java
@@ -20,56 +20,50 @@ import com.example.employee.service.EmployeeService;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    @Autowired
-    private EmployeeRepository empRepo;
+   @Autowired
+	private EmployeeRepository empRepo;
 
-    @Autowired
-    private RestTemplate restTemplate;
-    
-    @Autowired
+	@Autowired
 	private AddressClient addressClient;
 
-    private Logger logger=LoggerFactory.getLogger(EmployeeServiceImpl.class);
-    
-    
-    @Override
-    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        Employee employee = UserMapper.mapToEmployee(employeeDto);
-        Employee savedEmployee = empRepo.save(employee);
-        return UserMapper.mapToEmployeeDto(savedEmployee);
-    }
+	private Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
-   
+	@Override
+	public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+		Employee employee = UserMapper.mapToEmployee(employeeDto);
+		Employee savedEmployee = empRepo.save(employee);
+		logger.info("employee is added ");
+		return UserMapper.mapToEmployeeDto(savedEmployee);
+	}
 
 	@Override
 	public List<EmployeeDto> getAllEmployees() {
 		// TODO Auto-generated method stub
-		List<Employee> employees=empRepo.findAll();
-		List<EmployeeDto> employeesDto=UserMapper.mapToEmployeeDto(employees);
+		List<Employee> employees = empRepo.findAll();
+		List<EmployeeDto> employeesDto = UserMapper.mapToEmployeeDto(employees);
+		logger.info("fetch all employees from DB");
 		return employeesDto;
 	}
 
+	// Feign Client
 
-	// Feignclient 
 	@Override
+	@Cacheable(cacheNames="Employee",key="#id")
 	public EmployeeDto getEmployeeById(long id) throws Exception {
 		// TODO Auto-generated method stub
-		Optional<Employee> employee=empRepo.findById(id);
-		if(employee.isPresent()) {
+		Optional<Employee> employee = empRepo.findById(id);
+		if (employee.isPresent()) {
 			EmployeeDto empDto = UserMapper.mapToEmployeeDto(employee.get());
-			//http://localhost:8081/api/employee/252
-			List<AddressDto> forObject=addressClient.getAddressByEmployeeId(empDto.getEmpId());
+			// http://localhost:8081/api/employee/252
+			List<AddressDto> forObject = addressClient.getAddressByEmployeeId(empDto.getEmpId());
 			logger.info("{}", forObject);
 			empDto.setAddresses(forObject);
+			logger.info("fetch employee from DB");
 			return empDto;
-		}else {
+		} else {
 			throw new Exception("Employee not found");
 		}
-		
-		
 	}
-
-
 }
 
 
